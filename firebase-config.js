6a5c0644fb70a158c2ceca81c373dbad83cfdc8f
kishor18171 <kishorekvworkholic@gmail.com>
2026-04-04 20:49:23 +0530

    Refactor Firebase initialization code

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-// Import the functions you need from the SDKs you need
-// (Compat libraries are already loaded in HTML)
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAUct1BRu4sHeVBngZORP7RKJLlhzlZCpo",
   authDomain: "college-event-website-78bc5.firebaseapp.com",
@@ -12,7 +8,6 @@ const firebaseConfig = {
   measurementId: "G-TJ4SFB7LL5"
 };
 
-// Initialize Firebase using the compat API so existing code doesn't break
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
